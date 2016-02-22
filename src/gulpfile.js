0bcc03a78607cb0445e03d053c9df7edd2061f79
Daniel Dorrity <dorritydj@gmail.com>
2016-02-21 21:03:39 -0500

    Started Gulp/Jasmine Automation

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,3 +1,22 @@
 /**
  * Created by dorritydj on 2/14/16.
  */
+var gulp = require('gulp');
+var inject = require('gulp-inject');
+var jasmine = require('gulp-jasmine');
+
+gulp.task('inject', function(){
+    var sources = gulp.src([
+        'node_modules/angular/angular.js',
+        'webapp/**/*.js'
+    ]);
+
+    gulp.src('index.html')
+        .pipe(inject(sources, {relative:true}))
+        .pipe(gulp.dest(""));
+});
+
+gulp.task('tests', function(){
+    gulp.src('tests/*.spec.js')
+        .pipe(jasmine());
+});
