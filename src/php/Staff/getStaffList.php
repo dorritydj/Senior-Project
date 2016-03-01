88b4cf708bd13ca6ad971a389725f4752b6ea391
arc12912 <shadow12913@gmail.com>
2016-03-01 14:20:59 -0500

    Added files via upload

---
+++ src/php/Staff/getStaffList.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('display_errors', 1);
+require dirname(__FILE__).'/connection.php';
+
+
+// Check connection
+if ($conn->connect_error)
+{
+  $error = array('Error',$conn->connect_error)
+  return json_encode($error);
+  die("Connection failed: " . $conn->connect_error);
+} 
+else
+{
+	$sql = "SELECT staffName FROM Staff";
+	$result = mysqli_query($conn, $sql);
+	$rows = array();
+	while($r = mysqli_fetch_assoc($result)) 
+	{
+    	$rows[] = $r;
+	}
+	echo json_encode($rows);
+
+    mysqli_close($conn);
+}
+?>
